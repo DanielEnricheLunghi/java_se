d4c0e8b26fac0e6ad7b486b320a202294c2a177a
Daniel Enriche Lunghi <danielenrichelg@gmail.com>
2025-05-23 15:01:46 +0200

    Inizio tracciamento solo su src

---
+++ day_01/HelloWorld.java
@@ -0,0 +1,14 @@
+package day_01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 0; i <= 10; i++) {
+			System.out.println("salve");
+		}
+
+	}
+
+}
